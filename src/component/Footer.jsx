70fd3eed4545db1ef8a56d7040f1e548da5bbae2
0uali-Yassine <yassinzerouali0@gmail.com>
2024-10-09 15:03:37 +0200

    add footer section

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='p-4 afacad-flux' style={{backgroundColor:'#FFEDD2'}}>
+      <p className='text-center'>Built and designed by <span className='abril-fatface-regular' style={{color:'#294129',cursor:'pointer'}}>Yassine Zeouali.</span></p>
+      <p className='text-center'>All rights reserved. ©</p>
+    </div>
   )
 }
 
