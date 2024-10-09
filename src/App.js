c2a75223d2cba5519c066db7a59437fcb0e48566
0uali-Yassine <yassinzerouali0@gmail.com>
2024-10-09 15:01:35 +0200

    add extra code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import About from "./component/About";
+import Footer from "./component/Footer";
 import Home from "./component/Home";
+import Work from "./component/Work";
+
 
 function App() {
+  
+  const [data,setData] = useState([]);
+  
+  const fecthData = async()=>{
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    console.log(data);
+  }
+  fecthData();
   return (
     <div>
 
       <Home/>
+      <About/>
+      < Work/>
+      <Footer/>
       
     </div>
   );
